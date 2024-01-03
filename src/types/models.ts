15ecb297d2b501179dd22f67cd9fb2e81116546c
nephrite <oneabove232@gmail.com>
2024-01-03 21:26:08 +0800

    refactor: data types modeled into zod schema

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,4 +1,5 @@
-import { ObjectId, Document } from 'mongoose';
+import { ObjectId, Document, isValidObjectId } from 'mongoose';
+import { z } from 'zod';
 
 export type BaseDocument<T> = T & Document;
 
@@ -6,78 +7,86 @@ export type P<T> = Promise<T>
 
 type Id = {_id: ObjectId}
 
-export type BookArgs = {
-    title: string;
-    authors: string[];
-    yearPublished: number;    
+function objectIdValidator(value:unknown) {
+    return isValidObjectId(value)
 }
 
+const zodOIDValidator = z.custom<ObjectId>(objectIdValidator)
+const positiveIntNumber = z.number().int().positive()
+    
+
+export const bookArgsSchema = z.object({
+    title: z.string(),
+    authors: z.array(z.string()),
+    yearPublished: positiveIntNumber
+}).required()
+export type BookArgs = z.infer<typeof bookArgsSchema>
 export interface BookSchemaType extends BookArgs, Id {
     dateAdded: number;
 }
 
-
-export type MemberArgs = {
-    name: string;
-    age: number;
-}
-
+export const membersArgsSchema = z.object({
+    name: z.string(),
+    age: positiveIntNumber
+}).required()
+export type MemberArgs = z.infer<typeof membersArgsSchema>
 export interface MemberSchemaType extends MemberArgs, Id {
     joinDate: number;
     approvedBy: ObjectId;
 }
 
-export type BorrowArgs = {
-    bookId: ObjectId;
-    title: string;
-    borrower: ObjectId;
-    promisedReturnDate: number;
-}
-
+export const BorrowArgsSchema = z.object({
+    bookId: zodOIDValidator,
+    title: z.string(),
+    borrower: zodOIDValidator,
+    promisedReturnDate: positiveIntNumber
+}).required()
+export type BorrowArgs = z.infer<typeof BorrowArgsSchema>
 export interface BorrowSchemaType extends BorrowArgs, Id {
     approvedBy: ObjectId;
     date: number;
 }
 
 
-
-export type InventoryArgs = {
-    title: string;
-    total: number;
-    available: number;
-    borrowed: number;
-}
-
+export const InventoryArgsSchema = z.object({
+    title: z.string(),
+    total: positiveIntNumber,
+    available: positiveIntNumber,
+    borrowed: positiveIntNumber
+}).required()
+export type InventoryArgs = z.infer<typeof InventoryArgsSchema>
 export interface InventorySchemaType extends InventoryArgs, Id {}
 
-export type EmployeeArgs = {
-    name: string;
-    age: number;
-    email: string;
-    password: string;
-}
 
+export const EmployeeArgsSchema = z.object({
+    name: z.string(),
+    age: positiveIntNumber,
+    email: z.string().email(),
+    password: z.string()
+}).required()
+export type EmployeeArgs = z.infer<typeof EmployeeArgsSchema>
 export interface EmployeeSchemaType extends EmployeeArgs, Id {
     joinDate: number;
 }
 
 
-export type ReturnArgs =  {
-    bookId: ObjectId;
-    borrower: ObjectId;
-    borrowDate: number;    
-}
-
+export const ReturnArgsSchema = z.object({
+    bookId: zodOIDValidator,
+    borrower: zodOIDValidator,
+    borrowDate: positiveIntNumber    
+}).required()
+export type ReturnArgs = z.infer<typeof ReturnArgsSchema>
 export interface ReturnSchemaType extends ReturnArgs, Id {
     returnDate: number;
     approvedBy: ObjectId
 }
 
-export type PenaltyArgs = {
-    bookId: ObjectId;
-    borrower: ObjectId;
-    penalty: number;    
-}
+export const PenaltyArgsSchema = z.object({
+    bookId: zodOIDValidator,
+    borrower: zodOIDValidator,
+    penalty: positiveIntNumber,
+})
+export type PenaltyArgs = z.infer<typeof PenaltyArgsSchema>
 
 export interface PenaltySchemaType extends PenaltyArgs, Id {
     approvedBy: ObjectId;
