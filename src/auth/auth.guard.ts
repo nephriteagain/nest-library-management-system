c846333b251f080876c0b71428a183f671ae3fc2
nephrite <oneabove232@gmail.com>
2024-01-15 12:25:41 +0800

    fix: auth guard allowing any endpoint

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -16,28 +16,31 @@ export class AuthGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request: Request = context.switchToHttp().getRequest();
         const response: Response = context.switchToHttp().getResponse();
-
         if (request.path === '/') {
             console.log('serving static files');
             return true;
         }
 
         // NOTE: this is safe because there is a password required in the request body
-        if (request.path === '/api/auth/register' || 'api/auth/login') {
+        if (
+            request.path === '/api/auth/register' || 
+            request.path === ('api/auth/login')
+        ) {
+            console.log(request.path, 'is allowed')
             return true;
         }
 
-        if (
-            envConstants.regexRoutes.some((item) =>
-                item.regex.test(request.path),
-            )
-        ) {
-            console.log('spa routes matched, interceptor will handle it');
-            return true;
-        }
+        // if (
+        //     envConstants.regexRoutes.some((item) =>
+        //         item.regex.test(request.path),
+        //     )
+        // ) {
+        //     console.log('spa routes matched, interceptor will handle it');
+        //     return true;
+        // }
 
         if (request.path.startsWith('/api/auth') && request.method !== 'GET') {
-            console.log('auth guard skipped');
+            console.log('GET on auth, guard skipped');
             return true;
         }
 
